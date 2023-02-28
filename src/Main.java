1c7f754aefca9d08cf39913e0b05c1291ad8f8aa
PupilCurious <ertugrul.bilal78@gmail.com>
2023-02-28 06:21:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayi Gir: ");
+        number = input.nextInt();
+        if(prime(number,number/2)){
+            System.out.println(number + " Asal Sayidir...");
+        }else{
+            System.out.println(number + " Asal Degildir...");
+        }
+    }
+    public static boolean prime(int number, int divider){
+        if(number<2){
+            return false;
+        }else if(divider==1){
+            return true;
+        }else{
+            if(number % divider==0){
+                return false;
+            }else {
+                return prime(number, divider-1);
+            }
+
+        }
+    }
+}
